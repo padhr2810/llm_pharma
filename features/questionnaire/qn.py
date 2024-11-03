9987aff4a8041344a8fe80b52b457e2d9680a7ba
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-11-03 11:18:03 +0000

    Update qn.py

--- features/questionnaire/qn.py
+++ features/questionnaire/qn.py
@@ -1,1 +1,8 @@
-pass
+
+
+questions = {
+    "Age" : "What is your age?",
+    "sex" : ["What is your sex?", ["Male", "Female"]],
+    "Diagnoses" : "Do you have any medical diagnosis? Please list any diagnosis here",
+    "Medication": "Do you take any medicines? Please list your medicines here."
+}

8f3830dfe004bbf2c73146751f9a6b99ad0bf739
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-11-03 11:39:13 +0000

    Add files via upload

--- features/questionnaire/qn.py
+++ features/questionnaire/qn.py
@@ -1,8 +1,22 @@
 
+import gradio as gr
 
-questions = {
+q = {
     "Age" : "What is your age?",
     "sex" : ["What is your sex?", ["Male", "Female"]],
     "Diagnoses" : "Do you have any medical diagnosis? Please list any diagnosis here",
     "Medication": "Do you take any medicines? Please list your medicines here."
 }
+
+
+import gradio as gr
+
+with gr.Blocks() as demo:
+    text_meds = gr.Textbox()
+    text_diag = gr.Textbox()
+    text_age  = gr.Textbox()
+    choose_sex = gr.Dropdown(label="Sex", choices=["Male", "Female"])
+
+demo.launch()
+
+

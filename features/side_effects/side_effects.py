361c2726873177c523e5a6fc64b9d81131d3fec6
Padhraig <8850767+padhr2810@users.noreply.github.com>
2024-11-04 11:17:48 +0000

    Update side_effects.py

--- features/side_effects/side_effects.py
+++ features/side_effects/side_effects.py
@@ -1,3 +1,10 @@
 
 # https://github.com/raokarthik99/Efficient-polypharmacy-side-effects-prediction-GCNs
 
+# https://www.kaggle.com/datasets/jithinanievarghese/drugs-side-effects-and-medical-condition
+
+# http://snap.stanford.edu/decagon/
+
+# Naranjo Algorithm - couldn't find anything on github - https://pmc.ncbi.nlm.nih.gov/articles/PMC10763692/
+
+
